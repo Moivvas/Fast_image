bd9ab8fcaa296dbc7f79d6541777bdbc82174690
Dmytro <777fesenko.d@gmail.com>
2023-12-17 14:04:39 +0200

    fix bugs in route

--- src/routes/comments.py
+++ src/routes/comments.py
@@ -28,26 +28,26 @@ async def create_comment(body: CommentModel, db: Session = Depends(get_db),
                          current_user: User = Depends(auth_service.get_current_user)):
     image = db.query(Image).filter_by(id=body.image_id).first()
     if not image:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such image")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, messages="No such image")
     comment = await repository_comments.create_comment(body, current_user, db)
     return comment
 
 
 @router.get('/{comment_id}', response_model=CommentResponse)
 async def get_comment_by_id(comment_id: int = Path(ge=1), db: Session = Depends(get_db),
-                            _: User = Depends(auth_service.get_current_user)):
+                            current_user: User = Depends(auth_service.get_current_user)):
     comment = await repository_comments.get_comment_by_id(comment_id, db)
     if not comment:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such comment")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, messages="No such comment")
     return comment
 
 
 @router.get('/image/{image_id}', response_model=List[CommentResponse])
 async def get_comment_by_image_id(image_id: int = Path(ge=1), db: Session = Depends(get_db),
-                                  _: User = Depends(auth_service.get_current_user)):
+                                  current_user: User = Depends(auth_service.get_current_user)):
     comment = await repository_comments.get_comments_for_photo(image_id, db)
     if not comment:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such photo")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, messages="No such photo")
     return comment
 
 
@@ -62,5 +62,5 @@ async def update_comment(body: CommentModelUpdate, db: Session = Depends(get_db)
 async def remove_comment(comment_id: int = Path(ge=1), db: Session = Depends(get_db)):
     comment = await repository_comments.remove_comment(comment_id, db)
     if not comment:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such comment")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, messages="No such comment")
     return comment
