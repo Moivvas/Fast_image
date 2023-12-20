4a2cc5479bca1461fb6f8b132ea9414cf058ba4b
Moivvas <130219729+Moivvas@users.noreply.github.com>
2023-12-20 17:55:12 +0200

    true2

--- src/conf/config.py
+++ src/conf/config.py
@@ -2,11 +2,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
-    postgres_db: str = 'db'
-    postgres_user: str = 'some_user'
-    postgres_password: str = 'password'
-    postgres_port: int = '8000'
     sqlalchemy_database_url: str = 'postgresql+psycopg2://user:password@localhost:5432/postgres'
     secret_key: str = 'secret_key'
     algorithm: str = 'HS256'
@@ -16,6 +11,12 @@ class Settings(BaseSettings):
     cloudinary_name: str = 'Cloudinary'
     cloudinary_api_key: int = '12903053'
     cloudinary_api_secret: str = 'secret'
+    postgres_db: str = 'db'
+    postgres_user: str = 'some_user'
+    postgres_password: str = 'password'
+    postgres_port: int = '8000'
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
    
 
 settings = Settings()
